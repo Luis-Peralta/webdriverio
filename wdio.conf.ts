0794605d1b8d93efb909b8e3f4ea4964c2ef5d34
Luis Peralta <luis_peralta_000@hotmail.com>
2025-09-08 13:38:18 -0300

    fix pipe

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -11,7 +11,7 @@ const customConfig = {
 function getDockerConfig() {
   if (process.env.RUN_WITH_DOCKER === 'true') {
     return {
-      hostname: 'chrome',
+      hostname: 'localhost',
       port: 4444,
       path: '/wd/hub',
     };
